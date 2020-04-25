884eeb1d40a702fd457f29eae048b9a8a0269046
Stephen McBride <stevemcbride3@gmail.com>
2020-04-25 02:49:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,6 +4,8 @@ class Anagram
   attr_accessor :word: :possible_anagrams
 
   def initialize(word, possible_anagrams)
+    @word = word
+    @possible_anagrams = possible_anagrams
   end
 
   def detect_anagrams
